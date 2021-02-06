f6b1ed96f11c4b74680541b64caca0d588ce111d
Luca Barone <baro.luc@gmail.com>
2021-02-06 01:15:23 +0100

    :sparkles: Add Player Scene (#1)
    * :sparkles: Add Player Scene
    
    * :truck: Group Player files into a module folder
    
    * Level init

---
+++ src/Actor.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+class_name Actor
+
+export var gravity := 3000.0
+export var speed := Vector2(300.0, 1000.0)
+var velocity := Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	velocity.y = max(velocity.y + gravity * delta, speed.y)
+	velocity = move_and_slide(velocity)
